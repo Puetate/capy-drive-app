f2dc43917a15b138d40bf24fd2a478884fbb0db6
Pablo <jhoelpablo02@gmail.com>
2024-01-13 14:35:43 -0500

    feat: add service delete user

---
+++ src/app/(protected)/admin/users/services/deleteUser.service.ts
@@ -0,0 +1,9 @@
+import { ResponseRequest } from "@/app/models/responseRequest.model";
+import { User } from "@/app/models/user.model";
+import API from "@/lib/axios/api";
+
+export async function deleteUserService(id: number) {
+    const url = `/users/${id}`;
+    const res = await API.del<ResponseRequest<User>>({ url: url})
+    return res
+} 
