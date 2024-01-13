fb67eee6a4f56f6f05110c5b3755e2621da20147
Pablo <jhoelpablo02@gmail.com>
2024-01-13 14:36:05 -0500

    feat: add service edit user

--- src/app/(protected)/admin/users/services/editUser.service.ts
+++ src/app/(protected)/admin/users/services/editUser.service.ts
@@ -4,6 +4,6 @@ import API from "@/lib/axios/api";
 
 export async function editUserService(id: string, user: User) {
     const url = `/users/${id}`;
-    const res = await API.patch<ResponseRequest<User>>({ url: url, data: user })
+    const res = await API.put<ResponseRequest<User>>({ url: url, data: user })
     return res
 } 
