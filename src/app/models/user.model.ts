f308f7a3f2c660240393c202890260ce456ee768
Pablo <jhoelpablo02@gmail.com>
2024-01-13 02:48:47 -0500

    feat: add models role, user

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,27 @@
+import { Role } from "./role.model";
+
+export enum Roles {
+	'SUPER-ADMIN' = "SUPER-ADMIN",
+	ADMIN = "ADMIN",
+	SECRETARY = "SECRETARY",
+}
+
+export enum State {
+	ACTIVE = "ACTIVO",
+	INACTIVE = "INACTIVO",
+}
+
+export const enumUserState = Object.values(State);
+
+export interface User {
+	id: number,
+	names: string,
+	surnames: string,
+	fullName?: string,
+	phone: string,
+	dni: string
+	email: string,
+	// status: boolean,
+	roles: Role[] | string[],
+	role?: string
+}
