48f11ec9a04282be272a0e9a19ee9e66eab11dfc
Pablo <jhoelpablo02@gmail.com>
2024-01-13 02:51:30 -0500

    feat: add get post and put services user

---
+++ src/app/(protected)/admin/users/services/getUsers.service.ts
@@ -0,0 +1,9 @@
+import { User } from "@/app/models/user.model";
+import API from "../../../../../lib/axios/api";
+import { ResponseRequest } from "@/app/models/responseRequest.model";
+
+export async function getUsersService() {
+    const url: string = "/users"
+    const res = await API.get<ResponseRequest<User[]>>({ url });
+    return res;
+}
