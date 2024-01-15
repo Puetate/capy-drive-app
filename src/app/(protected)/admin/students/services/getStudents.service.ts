0493fd3fbd0f272491d08abfd07c342521e97529
Pablo <jhoelpablo02@gmail.com>
2024-01-15 16:38:04 -0500

    feat: change services

--- src/app/(protected)/admin/students/services/getStudents.service.ts
+++ src/app/(protected)/admin/students/services/getStudents.service.ts
@@ -3,7 +3,7 @@ import { Student } from "@/app/models/student.model";
 import { TemplateModel } from "@/app/models/templateModel.model";
 import API from "@/lib/axios/api";
 
-export default async function getStudentsService() {
+export default async function getStudentsService(careerId:string) {
     const url = "/students/1";
     const res = await API.get<ResponseRequest<Student[]>>({ url: url })
     return res
--- src/app/(protected)/admin/templates/services/getTemplates.service.ts
+++ src/app/(protected)/admin/templates/services/getTemplates.service.ts
@@ -2,8 +2,8 @@ import { ResponseRequest } from "@/app/models/responseRequest.model";
 import { TemplateModel } from "@/app/models/templateModel.model";
 import API from "@/lib/axios/api";
 
-export default async function getTemplatesService() {
-    const url = "/template";
-    const res = await API.get<ResponseRequest<TemplateModel>>({ url: url })
+export default async function getTemplatesService(careerID:string) {
+    const url = `/templates/${careerID}`;
+    const res = await API.get<ResponseRequest<TemplateModel[]>>({ url: url })
     return res
 }
